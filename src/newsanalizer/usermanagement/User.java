36eb0f1e8b3809d4af6e52a2b71a80bac361bd71
MadalinDanut <danutmadalin.md@gmail.com>
2023-02-02 22:38:40 +0200

    Initial commit

---
+++ src/newsanalizer/usermanagement/User.java
@@ -0,0 +1,62 @@
+package newsanalizer.usermanagement;
+
+import java.util.Objects;
+
+public class User {
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
+    }
+
+    public boolean isAnalyst() {
+        return isAnalyst;
+    }
+
+    public void setAnalyst(boolean analyst) {
+        isAnalyst = analyst;
+    }
+
+    public boolean equalsUsers(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return username.equals(user.username) && password.equals(user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, password);
+    }
+    private boolean isAdmin;
+
+    private boolean isAnalyst;
+    private String username;
+    private String password;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
